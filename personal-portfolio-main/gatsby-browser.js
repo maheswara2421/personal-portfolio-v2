599837d31442f310e18b474fa2cdc1683dd5346c
M MOHAN S S N VIVEK <maheswara2421@gmail.com>
2025-09-27 12:46:01 +0530

    Delete personal-portfolio-main directory

--- personal-portfolio-main/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// Import TailwindCSS
-import './src/styles/tailwind.css';
--- personal-portfolio-main/src/utils/sr.js
+++
@@ -1,8 +0,0 @@
-let sr = null;
-
-if (typeof document !== 'undefined') {
-  const ScrollReveal = require('scrollreveal').default;
-  sr = ScrollReveal();
-}
-
-export default sr;
